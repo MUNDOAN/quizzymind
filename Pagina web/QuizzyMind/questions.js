b41b257eaa28050ba3a80cf7ca96ce9d985c490d
MUNDOAN <137326250+MUNDOAN@users.noreply.github.com>
2023-12-04 12:22:04 -0600

    Add files via upload

---
+++ Pagina web/QuizzyMind/questions.js
@@ -0,0 +1,332 @@
+let questions = [
+    {
+        numb: 1,
+        question: "¿Qué es un lenguaje de programación?",
+        answer: "B) Un conjunto de reglas gramaticales para escribir código.",
+        options: [
+            "A) Un conjunto de instrucciones para hacer café.",
+            "B) Un conjunto de reglas gramaticales para escribir código.",
+            "C) Un juego de palabras para comunicarse en la programación.",
+            "D) Una forma de arte digital"
+        ]
+    },
+    {
+        numb: 2,
+        question: "¿Cuál de los siguientes es un lenguaje de programación de alto nivel?",
+        answer: "C) Python.",
+        options: [
+           "A) Ensamblador.",
+           "B) C.",
+           "C) Python.",
+           "D) HTML."
+        ]
+    },
+    {
+        numb: 3,
+        question: "¿Qué es un bucle en programación?",
+        answer: "B) Una repetición de instrucciones.",
+        options: [
+          "A) Un error en el código.",
+          "B) Una repetición de instrucciones.",
+          "C) Una conexión entre dos computadoras.",
+          "D) Una variable numérica."
+        ]
+    },
+    {
+        numb: 4,
+        question: "¿Qué hace un condicional en programación?",
+        answer: "B) Ejecuta un bloque de código solo si se cumple una condición.",
+        options: [
+            "A) Repite un bloque de código.",
+            "B) Ejecuta un bloque de código solo si se cumple una condición.",
+            "C) Imprime mensajes en la consola.",
+            "D) Define una función."
+        ]
+    },
+    {
+        numb: 5,
+        question: "¿Qué significa HTML en desarrollo web?",
+        answer: "A) HyperText Markup Language.",
+        options: [
+            "A) HyperText Markup Language.",
+            "B) High-Level Text Manipulation Language.",
+            "C) Human Task Management Language.",
+            "D) Hyperlink and Text Markup Language."
+        ]
+    },
+    {
+        numb: 6,
+        question: "¿Cuál es el propósito principal de CSS en desarrollo web?",
+        answer: "B) Dar estilo y diseño a las páginas web.",
+        options: [
+            "A) Manejar la lógica del lado del servidor.",
+            "B) Dar estilo y diseño a las páginas web.",
+            "C) Crear bases de datos.",
+            "D) Ejecutar scripts en el navegador."
+        ]
+    },
+    {
+        numb: 7,
+        question: "¿Cuál es el símbolo comúnmente utilizado para comentarios en muchos lenguajes de programación?",
+        answer: "A) //",
+        options: [
+           "A) //",
+           "B) --",
+           "C) **",
+           "D) ##"
+        ]
+    },
+    {
+        numb: 8,
+        question: "¿Qué es un array en programación?",
+        answer: "B) Una colección de elementos del mismo tipo.",
+        options: [
+          "A) Una función matemática.",
+          "B) Una colección de elementos del mismo tipo.",
+          "C) Un tipo de bucle.",
+          "D) Un valor booleano."
+        ]
+    },
+    {
+        numb: 9,
+        question: "¿Qué es un IDE?",
+        answer: "B) Un entorno de desarrollo integrado.",
+        options: [
+            "A) Un dispositivo de entrada.",
+            "B) Un entorno de desarrollo integrado.",
+            "C) Una etiqueta HTML.",
+            "D) Una función matemática avanzada."
+        ]
+    },
+    {
+        numb: 10,
+        question: "¿Cuál de las siguientes es una variable?",
+        answer: "C) x = 5.",
+        options: [
+            "A) 10.",
+            "B) Hola.",
+            "C) x = 5.",
+            "D) D) True."
+        ]
+    },
+    {
+        numb: 11,
+        question: "¿Cuál de las siguientes NO es un tipo de dato en programación?",
+        answer: "C) Loop.",
+        options: [
+            "A) String.",
+            "B) Float.",
+            "C) Loop.",
+            "D) Boolean."
+        ]
+    },
+    {
+        numb: 12,
+        question: "¿En qué consiste la programación orientada a objetos?",
+        answer: "B) Utiliza objetos para organizar y estructurar el código.",
+        options: [
+           "A) Organiza el código en bloques de texto.",
+           "B) Utiliza objetos para organizar y estructurar el código.",
+           "C) Genera código automáticamente.",
+           "D) Crea páginas web dinámicas."
+        ]
+    },
+    {
+        numb: 13,
+        question: "¿Qué es un algoritmo?",
+        answer: "B) Una secuencia de pasos para realizar una tarea.",
+        options: [
+          "A) Un lenguaje de programación.",
+          "B) Una secuencia de pasos para realizar una tarea.",
+          "C) Un error en el código.",
+          "D) Una variable global."
+        ]
+    },
+    {
+        numb: 14,
+        question: "¿Cuál es el operador lógico para la negación (NOT) en muchos lenguajes de programación?",
+        answer: "C) !",
+        options: [
+            "A) &&",
+            "B) ||",
+            "C) !",
+            "D) #"
+        ]
+    },
+    {
+        numb: 15,
+        question: "¿Qué es Git en el contexto de desarrollo de software?",
+        answer: "C) Un sistema de control de versiones.",
+        options: [
+            "A) Un lenguaje de programación.",
+            "B) Una plataforma de redes sociales para desarrolladores.",
+            "C) Un sistema de control de versiones.",
+            "D) Un tipo de base de datos."
+        ]
+    },
+    {
+        numb: 16,
+        question: "¿Cuál es el propósito principal de un constructor en programación orientada a objetos?",
+        answer: "C) Inicializar objetos de la clase.",
+        options: [
+            "A) Definir una clase.",
+            "B) Crear una instancia de la clase.",
+            "C) Inicializar objetos de la clase.",
+            "D) Realizar operaciones matemáticas."
+        ]
+    },
+    {
+        numb: 17,
+        question: "¿Qué es una API (Interfaz de Programación de Aplicaciones)?",
+        answer: "C) Un conjunto de funciones y reglas para interactuar con un software o servicio.",
+        options: [
+           "A) Un conjunto de reglas gramaticales para escribir código.",
+           "B) Un entorno de desarrollo integrado.",
+           "C) Un conjunto de funciones y reglas para interactuar con un software o servicio.",
+           "D) Una base de datos en la nube."
+        ]
+    },
+    {
+        numb: 18,
+        question: "¿Cuál es el propósito de la instrucción (import) en muchos lenguajes de programación?",
+        answer: "B) Incluir bibliotecas o módulos externos.",
+        options: [
+          "A) Exportar datos a un archivo.",
+          "B) Incluir bibliotecas o módulos externos.",
+          "C) Crear una nueva variable.",
+          "D) Imprimir mensajes en la consola."
+        ]
+    },
+    {
+        numb: 19,
+        question: "¿Qué es DevOps en el desarrollo de software?",
+        answer: "B) Una práctica que integra desarrollo y operaciones.",
+        options: [
+            "A) Una metodología para el desarrollo de videojuegos.",
+            "B) Una práctica que integra desarrollo y operaciones.",
+            "C) Un lenguaje de programación.",
+            "D) Una plataforma de diseño gráfico."
+        ]
+    },
+    {
+        numb: 20,
+        question: "¿Cuál es la función de un gestor de paquetes en programación?",
+        answer: "B) Instalar y gestionar bibliotecas y dependencias de un proyecto.",
+        options: [
+            "A) Gestionar el rendimiento de la computadora.",
+            "B) Instalar y gestionar bibliotecas y dependencias de un proyecto.",
+            "C) Crear interfaces de usuario.",
+            "D) Realizar pruebas de código automáticamente."
+        ]
+    },
+    {
+        numb: 21,
+        question: "¿Qué es JavaScript?",
+        answer: "A) Un lenguaje de programación.",
+        options: [
+            "A) Un lenguaje de programación.",
+            "B) Un tipo de café.",
+            "C) Un software de diseño gráfico.",
+            "D) Un sistema operativo."
+        ]
+    },
+    {
+        numb: 22,
+        question: "¿Qué es una variable en programación?",
+        answer: "C) Un contenedor para almacenar información.",
+        options: [
+           "A) Una instrucción de impresión.",
+           "B) Un valor constante.",
+           "C) Un contenedor para almacenar información.",
+           "D) Una función matemática."
+        ]
+    },
+    {
+        numb: 23,
+        question: "¿Qué es un bucle (for) en programación?",
+        answer: "B) Una repetición de instrucciones.",
+        options: [
+          "A) Una condición.",
+          "B) Una repetición de instrucciones.",
+          "C) Un tipo de variable.",
+          "D) Una función matemática."
+        ]
+    },
+    {
+        numb: 24,
+        question: "¿En qué se diferencia una lista de un diccionario en Python?",
+        answer: "C) Las listas son ordenadas, los diccionarios no tienen un orden definido.",
+        options: [
+            "A) Las listas son inmutables, los diccionarios no.",
+            "B) Las listas contienen pares clave-valor, los diccionarios no.",
+            "C) Las listas son ordenadas, los diccionarios no tienen un orden definido.",
+            "D) Las listas solo pueden contener números, los diccionarios pueden contener cualquier tipo de dato."
+        ]
+    },
+    {
+        numb: 25,
+        question: "¿Cuál es el propósito de CSS en desarrollo web?",
+        answer: "B) Dar estilo y diseño a las páginas web.",
+        options: [
+            "A) Manejar la lógica del lado del servidor.",
+            "B) Dar estilo y diseño a las páginas web.",
+            "C) Crear bases de datos.",
+            "D) Controlar la interactividad del usuario."
+        ]
+    },
+    {
+        numb: 26,
+        question: "¿Qué significa HTML?",
+        answer: "A) HyperText Markup Language.",
+        options: [
+            "A) HyperText Markup Language.",
+            "B) High-Level Text Manipulation.",
+            "C) HyperTransfer Markup Language.",
+            "D) Human Text Modeling Language."
+        ]
+    },
+    {
+        numb: 27,
+        question: "¿Qué es Git?",
+        answer: "B) Un sistema de control de versiones.",
+        options: [
+           "A) Un lenguaje de programación.",
+           "B) Un sistema de control de versiones.",
+           "C) Un tipo de base de datos.",
+           "D) Un software de diseño gráfico."
+        ]
+    },
+    {
+        numb: 28,
+        question: "¿Cuál es el símbolo para comentarios en JavaScript?",
+        answer: "A) //",
+        options: [
+          "A) //",
+          "B) --",
+          "C) #",
+          "D) /* */"
+        ]
+    },
+    {
+        numb: 29,
+        question: "¿Qué significa API en programación?",
+        answer: "A) Application Programming Interface.",
+        options: [
+            "A) Application Programming Interface.",
+            "B) Advanced Programming Instruction.",
+            "C) Application Process Integration.",
+            "D) Automated Programming Interface.."
+        ]
+    },
+    {
+        numb: 30,
+        question: "¿Cuál es la función principal de un constructor en programación orientada a objetos?",
+        answer: "A) Inicializar objetos de la clase.",
+        options: [
+            "A) Inicializar objetos de la clase.",
+            "B) Crear una nueva clase.",
+            "C) Imprimir mensajes en la consola.",
+            "D) Definir una interfaz de usuario."
+        ]
+    },
+];
